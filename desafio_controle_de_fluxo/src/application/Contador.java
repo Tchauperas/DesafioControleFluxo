6e678c2d3c7ac11cf8dcfb76df6e547ee7631e51
Marcos_Vhynycyus <121253173+Tchauperas@users.noreply.github.com>
2024-09-04 20:07:17 -0300

    commit

--- desafio_controle_de_fluxo/src/application/Contador.java
+++ desafio_controle_de_fluxo/src/application/Contador.java
@@ -3,24 +3,40 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import util.ParametrosInvalidos;
+import util.ParametrosInvalidosException;
 
 public class Contador {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		boolean check = true;
 		
 		System.out.print("Digite o primeiro parâmetro: ");
 		int p1 = sc.nextInt();
 		System.out.print("Digite o segundo parâmetro: ");
 		int p2 = sc.nextInt();
 		
-		try {
-			
-		} catch (ParametrosInvalidos e) {
-			
+		while(check) {
+			try {
+				
+				contar(p1, p2);
+				check = false;
+				
+			} catch (Exception e) {
+				
+				System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+				
+			}
 		}
 		
+		System.out.println("Done.");
+		
 		sc.close();
+	}
+	
+	public static void contar(int p1, int p2) throws ParametrosInvalidosException {
+		if(p1 > p2) {
+			throw new ParametrosInvalidosException();
+		}
 	}
 }
---
+++ desafio_controle_de_fluxo/src/util/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package util;
+
+public class ParametrosInvalidosException {
+	
+	public ParametrosInvalidosException() {
+		super();
+	}
+	
+	
+}
